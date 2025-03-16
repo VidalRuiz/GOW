aa4fadaefb5ffef5bc11ed105892adbbde9e3d5a
Vidal Ruiz <vidalruiz@me.com>
2025-03-15 22:55:54 -0600

    Array de videojuegos con anio de lanzamiento

--- GOW/Controller/GamesViewController.swift
+++ GOW/Controller/GamesViewController.swift
@@ -1,28 +1,103 @@
 import UIKit
 
+/// Estructura que representa un juego de Gears of War
+struct Game {
+    let title: String
+    let releaseYear: Int
+    let imageName: String
+}
+
 class GamesViewController: UIViewController {
     
-    @IBOutlet weak var gameImage: UIImageView!
-    @IBOutlet weak var imagePageControl: UIPageControl!
+    /// Lista de juegos con sus datos
+    let games: [Game] = [
+        Game(title: "Gears of War", releaseYear: 2006, imageName: "0"),
+        Game(title: "Gears of War 2", releaseYear: 2008, imageName: "1"),
+        Game(title: "Gears of War 3", releaseYear: 2011, imageName: "2"),
+        Game(title: "Gears of War 4", releaseYear: 2016, imageName: "3"),
+        Game(title: "Gears 5", releaseYear: 2019, imageName: "4"),
+        Game(title: "Gears of War Judgment", releaseYear: 2013, imageName: "5"),
+        Game(title: "Gears Tactics", releaseYear: 2020, imageName: "6"),
+        Game(title: "Gears of War Ultimate Edition", releaseYear: 2015, imageName: "7")
+    ]
     
-    // Data source for game images
-    let gamePosters = Array(0...7) // Image indexes corresponding to assets
+    // UI Elements
+    private let gameImage: UIImageView = {
+        let imageView = UIImageView()
+        imageView.contentMode = .scaleAspectFill
+        imageView.clipsToBounds = true
+        return imageView
+    }()
     
+    private let gameTitleLabel: UILabel = {
+        let label = UILabel()
+        label.textAlignment = .center
+        label.font = UIFont.boldSystemFont(ofSize: 24)
+        label.textColor = UIColor(named: "GOWText") ?? .white
+        return label
+    }()
+    
+    private let gameYearLabel: UILabel = {
+        let label = UILabel()
+        label.textAlignment = .center
+        label.font = UIFont.systemFont(ofSize: 18)
+        label.textColor = UIColor(named: "GOWText") ?? .lightGray
+        return label
+    }()
+    
+    private let imagePageControl: UIPageControl = {
+        let pageControl = UIPageControl()
+        pageControl.pageIndicatorTintColor = .gray
+        pageControl.currentPageIndicatorTintColor = .red
+        return pageControl
+    }()
+    
+    private var currentIndex = 0
+
     override func viewDidLoad() {
         super.viewDidLoad()
-        
-        // Configure the UIPageControl with the number of images available
-        imagePageControl.numberOfPages = gamePosters.count
-        
-        // Set the initial game image
-        gameImage.image = UIImage(named: gamePosters.first?.description ?? "0")
-        
-        // Configure swipe gestures programmatically
-        configureSwipeGestures()
+        setupUI()
+        configureGestures()
+        updateUI(for: currentIndex)
     }
     
-    /// Configures left and right swipe gestures for image navigation
-    private func configureSwipeGestures() {
+    /// Configura la UI
+    private func setupUI() {
+        view.backgroundColor = UIColor(named: "GOWBlack1") ?? .black
+        
+        // Agregar elementos a la vista
+        view.addSubview(gameImage)
+        view.addSubview(gameTitleLabel)
+        view.addSubview(gameYearLabel)
+        view.addSubview(imagePageControl)
+        
+        // Configurar constraints
+        gameImage.translatesAutoresizingMaskIntoConstraints = false
+        gameTitleLabel.translatesAutoresizingMaskIntoConstraints = false
+        gameYearLabel.translatesAutoresizingMaskIntoConstraints = false
+        imagePageControl.translatesAutoresizingMaskIntoConstraints = false
+        
+        NSLayoutConstraint.activate([
+            gameImage.topAnchor.constraint(equalTo: view.safeAreaLayoutGuide.topAnchor, constant: 20),
+            gameImage.centerXAnchor.constraint(equalTo: view.centerXAnchor),
+            gameImage.widthAnchor.constraint(equalTo: view.widthAnchor, multiplier: 0.9),
+            gameImage.heightAnchor.constraint(equalTo: view.heightAnchor, multiplier: 0.5),
+            
+            gameTitleLabel.topAnchor.constraint(equalTo: gameImage.bottomAnchor, constant: 20),
+            gameTitleLabel.centerXAnchor.constraint(equalTo: view.centerXAnchor),
+            
+            gameYearLabel.topAnchor.constraint(equalTo: gameTitleLabel.bottomAnchor, constant: 10),
+            gameYearLabel.centerXAnchor.constraint(equalTo: view.centerXAnchor),
+            
+            imagePageControl.topAnchor.constraint(equalTo: gameYearLabel.bottomAnchor, constant: 20),
+            imagePageControl.centerXAnchor.constraint(equalTo: view.centerXAnchor)
+        ])
+        
+        imagePageControl.numberOfPages = games.count
+    }
+    
+    /// Configura los gestos de Swipe
+    private func configureGestures() {
         let swipeRight = UISwipeGestureRecognizer(target: self, action: #selector(handleSwipe(_:)))
         swipeRight.direction = .right
         view.addGestureRecognizer(swipeRight)
@@ -32,18 +107,22 @@ class GamesViewController: UIViewController {
         view.addGestureRecognizer(swipeLeft)
     }
     
-    /// Handles swipe gestures to navigate through game posters
+    /// Maneja el cambio de imagen con Swipe
     @objc private func handleSwipe(_ gesture: UISwipeGestureRecognizer) {
-        var index = imagePageControl.currentPage
-        
         if gesture.direction == .right {
-            index = (index + 1) % gamePosters.count
+            currentIndex = (currentIndex + 1) % games.count
         } else if gesture.direction == .left {
-            index = (index - 1 + gamePosters.count) % gamePosters.count
+            currentIndex = (currentIndex - 1 + games.count) % games.count
         }
-        
-        // Update the displayed image and page control indicator
-        gameImage.image = UIImage(named: String(gamePosters[index]))
+        updateUI(for: currentIndex)
+    }
+    
+    /// Actualiza la UI con el juego actual
+    private func updateUI(for index: Int) {
+        let game = games[index]
+        gameImage.image = UIImage(named: game.imageName)
+        gameTitleLabel.text = game.title
+        gameYearLabel.text = "Año de lanzamiento: \(game.releaseYear)"
         imagePageControl.currentPage = index
     }
 }
--- GOW/Controller/HomeViewController.swift
+++ GOW/Controller/HomeViewController.swift
@@ -1,40 +1,22 @@
-//
-//  HomeViewController.swift
-//  GOW
-//
-//  Created by Rafael Gonzalez on 07/03/25.
-//
-
 import UIKit
 
 class HomeViewController: UITableViewController {
     
     @IBOutlet var menuTableView: UITableView!
     
-    let menuOptions : [MenuOption] = [
-            MenuOption(
-                title:"menu.option.videogames",
-                image:"gamecontroller.fill",
-                segue:"gamesSegue")
-            , MenuOption(
-                title:"menu.option.weapons",
-                image:"shield.fill",
-                segue:"weaponsSegue")
-            , MenuOption(
-                title:"menu.option.characters",
-                image:"person.crop.rectangle.stack.fill",
-                segue:"charactersSegue")
-            ,MenuOption(
-                title:"menu.option.merchandise",
-                image:"shippingbox.fill",
-                segue:"merchandiseSegue")
-        ]
+    let menuOptions: [MenuOption] = [
+        MenuOption(title: "menu.option.videogames", image: "gamecontroller.fill", segue: "gamesSegue"),
+        MenuOption(title: "menu.option.weapons", image: "shield.fill", segue: "weaponsSegue"),
+        MenuOption(title: "menu.option.characters", image: "person.crop.rectangle.stack.fill", segue: "charactersSegue"),
+        MenuOption(title: "menu.option.merchandise", image: "shippingbox.fill", segue: "merchandiseSegue")
+    ]
 
-
-    
     override func viewDidLoad() {
         super.viewDidLoad()
-
+        
+        // Configurar el color de fondo desde Assets.xcassets
+        view.backgroundColor = UIColor(named: "GOWBlack1")
+        menuTableView.backgroundColor = UIColor(named: "GOWBlack1")
     }
 
     // MARK: - Table view data source
@@ -48,18 +30,26 @@ class HomeViewController: UITableViewController {
     }
 
     override func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
-
-        // Configure the cell
         let cell = tableView.dequeueReusableCell(withIdentifier: "cell", for: indexPath) as! MenuOptionCell
-                // Configure the cell...
-        cell.menuLabel.text = NSLocalizedString(menuOptions[indexPath.row].title, comment: "");
-        cell.menuImage.image = UIImage(systemName:menuOptions[indexPath.row].image)
+
+        let menuOption = menuOptions[indexPath.row]
+
+        // Texto localizado
+        cell.menuLabel.text = NSLocalizedString(menuOption.title, comment: "")
+
+        // Colores
+        cell.backgroundColor = UIColor(named: "GOWBlack1") // Fondo negro
+        cell.menuLabel.textColor = UIColor(named: "GOWText") // Texto blanco o gris claro
+
+        // Configurar íconos en color blanco
+        if let icon = UIImage(systemName: menuOption.image) {
+            cell.menuImage.image = icon.withTintColor(UIColor(named: "GOWRed") ?? .white, renderingMode: .alwaysOriginal)
+        }
+
         return cell
     }
-    
+
     override func tableView(_ tableView: UITableView, didSelectRowAt indexPath: IndexPath) {
         performSegue(withIdentifier: menuOptions[indexPath.row].segue, sender: self)
     }
-    
-
 }
